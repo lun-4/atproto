3c9ce6b17d6ac4d7b01f8ee5b7ae04753b0c75ea
Luna <git@l4.pm>
2024-11-10 16:51:33 -0300

    move db migration to helper script1

--- luna/dataplane/src/bin.ts
+++ luna/dataplane/src/bin.ts
@@ -1,11 +1,13 @@
 import * as bsky from '@atproto/bsky'
 const run = async () => {
-  // shared across server, ingester, and indexer in order to share pool, avoid too many pg connections.
   const db = new bsky.Database({
     url: process.env.POSTGRES_URL || '',
     schema: process.env.POSTGRES_SCHEMA,
     poolSize: parseInt(process.env.POSTGRES_POOL_SIZE || '10', 10),
   })
+
+  // NOTE(luna): has to be done manually here, server doesn't migrate automatically
+  await db.migrateToLatestOrThrow()
 
   const dataplanePort = parseInt(process.env.DATAPLANE_PORT || '2671', 10)
   const dataplane = await bsky.DataPlaneServer.create(
