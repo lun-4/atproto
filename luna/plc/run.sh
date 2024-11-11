b39e0599aee12823bed2872a3195956398c29938
Luna <git@l4.pm>
2024-11-10 23:53:29 -0300

    add missing env vars for plc

--- luna/plc/run.sh
+++ luna/plc/run.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -eux
 cd ~/other.git/did-method-plc/packages/server/pg && docker-compose up -d db
+export DATABASE_URL="postgres://bsky:yksb@localhost:5466/plc_dev"
+export DEBUG_MODE=1
+export LOG_ENABLED="true"
+export LOG_LEVEL=debug
+export LOG_DESTINATION=1
 cd ~/other.git/did-method-plc/packages/server && yarn run start
