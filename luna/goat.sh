d6b9c7c628f5ede32aa6545ed524672a56e6d7a8
Luna <git@l4.pm>
2024-11-11 00:37:21 -0300

    fix goat.sh plc

--- luna/goat.sh
+++ luna/goat.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -eux
 export ATP_PDS_HOST=https://pds.gsky.ln4.net
-if [ "$1" = "plc" ]; then
+first_param=${1:-""}
+if [ "$first_param" = "plc" ]; then
   shift
   ~/other.git/indigo/goat plc -plc-directory https://plc.gsky.ln4.net $*
 else
