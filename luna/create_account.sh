77f20c06f195243e20f62bd6f15a332332a69e87
Luna <git@l4.pm>
2024-11-11 00:37:10 -0300

    fix script for account creation

--- luna/create_account.sh
+++ luna/create_account.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eux
-inv=$(./localinvite.sh | jq .code)
+inv=$(./localinvite.sh | jq -r .code)
 ./goat.sh account create --handle user1.pds.gsky.ln4.net --password 123 --email user1@example.net -invite-code "$inv"
 ./goat.sh account create --handle user2.pds.gsky.ln4.net --password 123 --email user2@example.net -invite-code "$inv"
 ./goat.sh account create --handle user3.pds.gsky.ln4.net --password 123 --email user3@example.net -invite-code "$inv"
--- luna/localinvite.sh
+++ luna/localinvite.sh
@@ -2,6 +2,6 @@ set -eux
 curl -k --request POST \
           --user "admin:2f28e4183b8ab1b5a684df9078483d74" \
           --header "Content-Type: application/json" \
-          --data '{"useCount": 1}' \
+          --data '{"useCount": 100}' \
           "https://pds.gsky.ln4.net/xrpc/com.atproto.server.createInviteCode"
 
