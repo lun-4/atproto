77bacd5191defb65ae4cb4ca2fae72ca5058a3ed
Luna <git@l4.pm>
2024-11-12 02:06:12 -0300

    fix dataplane plc url

--- luna/dataplane/run.sh
+++ luna/dataplane/run.sh
@@ -7,7 +7,7 @@ cat init.sql | docker-compose exec -T dataplane_db psql -U postgres
 export POSTGRES_URL="postgres://dataplane_bsky:35fe8e78fc60435@localhost:5858/bsky_dataplane?schema=public"
 export POSTGRES_SCHEMA=public
 export POSTGRES_POOL_SIZE=10
-export DID_PLC_URL=http://localhost:2582
+export DID_PLC_URL=https://plc.gsky.ln4.net
 export NODE_ENV=development
 export LOG_ENABLED=true
 export LOG_LEVEL=info
