2cb4c404963a1403bd6561b65910ce359e9a44ed
Luna <git@l4.pm>
2024-11-21 00:50:19 -0300

    use ATP_PLC_HOST patch

--- luna/goat.sh
+++ luna/goat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eux
 export ATP_PDS_HOST=https://pds.gsky.ln4.net
-export PLC_DIRECTORY_URL=https://plc.gsky.ln4.net
+export ATP_PLC_HOST=https://plc.gsky.ln4.net
 first_param=${1:-""}
 if [ "$first_param" = "plc" ]; then
   shift
