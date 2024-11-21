7b40155f0097338d7e830a2b6e5d758d83b5f564
Luna <git@l4.pm>
2024-11-21 20:20:36 -0300

    add JETSTREAM_LIVENESS_TTL

--- luna/jetstream/run.sh
+++ luna/jetstream/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eux
 export JETSTREAM_WS_URL=wss://relay.gsky.ln4.net/xrpc/com.atproto.sync.subscribeRepos
+export JETSTREAM_LIVENESS_TTL=9000000000s
 
 cd ~/other.git/jetstream && ./jetstream
